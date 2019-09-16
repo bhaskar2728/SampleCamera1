52fa67add4663703634c26484b65d5ec4e1b197a
bhaskar2728 <bhaskar18bcs@iiitkottayam.ac.in>
2019-09-16 20:51:51 +0530

    first commit

--- app/src/main/java/com/example/samplecamera/MainActivity.java
+++ app/src/main/java/com/example/samplecamera/MainActivity.java
@@ -34,5 +34,6 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         Bitmap bitmap = (Bitmap)data.getExtras().get("data");
         imgView.setImageBitmap(bitmap);
+        int x = 3;
     }
 }
